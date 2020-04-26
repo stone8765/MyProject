3acba7c15d4474a6699d7fd09ec13deb388532c3
StoneLi <lileisayno@live.com>
2020-04-26 15:58:52 +0800

    init project

--- my-project/.eslintrc.js
+++ my-project/.eslintrc.js
@@ -7,13 +7,16 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    // "@vue/prettier",
+    // "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
+    'quotes': [1, 'single'], //引号类型 `` "" ''
+    'semi': [0, 'never'], // 语句强制分号结尾
+    'space-before-function-paren': [0, 'always'], //函数定义时括号前面要不要有空格
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
